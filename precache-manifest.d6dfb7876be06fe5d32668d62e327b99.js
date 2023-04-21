d16d72cd846231ef0548bbff21d8eec074a2fb7b
manhhadtbk <56177308+manhhadtbk@users.noreply.github.com>
2023-04-21 11:16:28 +0700

    Updates

---
+++ precache-manifest.d6dfb7876be06fe5d32668d62e327b99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa6dccc46e713136b3b91d216db1b69",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "a57102788fdbfd1f54ec",
+    "url": "/todo-app/static/css/main.eb04b67f.chunk.css"
+  },
+  {
+    "revision": "86bfc19d3791ae940b35",
+    "url": "/todo-app/static/js/2.2614052b.chunk.js"
+  },
+  {
+    "revision": "8d98233dfb2f1a8b874a10cb1448b7bd",
+    "url": "/todo-app/static/js/2.2614052b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57102788fdbfd1f54ec",
+    "url": "/todo-app/static/js/main.df0372bd.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/todo-app/static/js/main.df0372bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e5b0b92004ad3f7a87",
+    "url": "/todo-app/static/js/runtime-main.edd2e9bf.js"
+  },
+  {
+    "revision": "62038fcbf10a494db9234247808b39d0",
+    "url": "/todo-app/static/media/times-solid.62038fcb.svg"
+  }
+]);
